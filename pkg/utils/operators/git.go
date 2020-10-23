9287c8f8d7cfd82d19451ea14168bc2ba561268e
neverCase <1024769485@qq.com>
2020-10-23 17:59:39 +0800

    pkg/uitls/operators/git*.go: adds functions

--- pkg/utils/operators/git.go
+++ pkg/utils/operators/git.go
@@ -3,10 +3,13 @@ package operators
 import (
 	"context"
 	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+
 	"github.com/nevercase/publisher/pkg/interfaces"
 	"github.com/nevercase/publisher/pkg/types"
 	"k8s.io/klog"
-	"os/exec"
 )
 
 func NewGit(gitDir string, branchName string) interfaces.StepOperator {
@@ -36,6 +39,16 @@ func (g *git) Step() *types.Step {
 func (g *git) Run() (res []string, err error) {
 	g.step.Phase = types.StepRunning
 	var out []byte
+	if out, err = g.cd(); err != nil {
+		klog.V(2).Info(err)
+		g.step.Phase = types.StepFailed
+		return res, err
+	}
+	if out, err = g.revert(); err != nil {
+		klog.V(2).Info(err)
+		g.step.Phase = types.StepFailed
+		return res, err
+	}
 	if out, err = g.pull(); err != nil {
 		klog.V(2).Info(err)
 		g.step.Phase = types.StepFailed
@@ -44,6 +57,50 @@ func (g *git) Run() (res []string, err error) {
 	res = append(res, string(out))
 	g.step.Phase = types.StepSucceeded
 	return res, nil
+}
+
+func (g *git) cd() (res []byte, err error) {
+	commands := fmt.Sprintf("cd %s", g.step.Envs[types.PublisherProjectDir])
+	return g.exec(commands)
+}
+
+func (g *git) branch() (res string, err error) {
+	commands := fmt.Sprintf("cd %s && git branch -a | grep '*'", g.step.Envs[types.PublisherProjectDir])
+	t, err := g.exec(commands)
+	if err != nil {
+		klog.V(2).Info(err)
+		return res, err
+	}
+	activeMatched, err := regexp.Match(`\*`, t)
+	if err != nil {
+		return res, fmt.Errorf("git regexp.Match active target-name:%s err:%v\n", g.step.Envs[types.PublisherGitBranch], err)
+	}
+	if activeMatched == false {
+		return res, fmt.Errorf("git regexp.Match active target-name:%s failed", g.step.Envs[types.PublisherGitBranch])
+	}
+	var name string
+	name = strings.Replace(string(t), " ", "", -1)
+	name = strings.Replace(name, "*", "", -1)
+	// such as `* test\n`
+	name = strings.Replace(name, "\n", "", -1)
+	return name, nil
+}
+
+func (g *git) fetchAll() (res []byte, err error) {
+	commands := fmt.Sprintf("cd %s && git fetch --all && git fetch -p", g.step.Envs[types.PublisherProjectDir])
+	return g.exec(commands)
+}
+
+func (g *git) revert() (res []byte, err error) {
+	commands := fmt.Sprintf("cd %s && git add --all && git checkout -f && git reset --hard", g.step.Envs[types.PublisherProjectDir])
+	return g.exec(commands)
+}
+
+func (g *git) checkout() (res []byte, err error) {
+	commands := fmt.Sprintf("cd %s && git checkout -B %s --track remotes/origin/%s",
+		g.step.Envs[types.PublisherProjectDir], g.step.Envs[types.PublisherGitBranch], g.step.Envs[types.PublisherGitBranch])
+	klog.Info("git checkout commands:", commands)
+	return g.exec(commands)
 }
 
 func (g *git) pull() (res []byte, err error) {
--- pkg/utils/operators/git_test.go
+++ pkg/utils/operators/git_test.go
@@ -13,6 +13,10 @@ var fakeGitOperator = NewGit("/Users/nevermore/go/src/github.com/Shanghai-Lunara
 
 var fakeErrGitOperator = NewGit("/Users/nevermore/go/src/github.com/Shanghai-Lunara/client-tool2", "main")
 
+var fakeGitOperator2 = NewGit("/Users/nevermore/go/src/github.com/Shanghai-Lunara/client-tool", "test")
+
+var fakeErrGitOperator2 = NewGit("/Users/nevermore/go/src/github.com/Shanghai-Lunara/client-tool", "test2xxxxx")
+
 func TestNewGit(t *testing.T) {
 	type args struct {
 		gitDir     string
@@ -165,6 +169,186 @@ func Test_git_exec(t *testing.T) {
 			//if !reflect.DeepEqual(gotRes, tt.wantRes) {
 			//	t.Errorf("git.exec() = %v, want %v", gotRes, tt.wantRes)
 			//}
+		})
+	}
+}
+
+func Test_git_cd(t *testing.T) {
+	type fields struct {
+		step *types.Step
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRes []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &git{
+				step: tt.fields.step,
+			}
+			gotRes, err := g.cd()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("git.cd() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("git.cd() = %v, want %v", gotRes, tt.wantRes)
+			}
+		})
+	}
+}
+
+func Test_git_fetchAll(t *testing.T) {
+	type fields struct {
+		step *types.Step
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRes []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &git{
+				step: tt.fields.step,
+			}
+			gotRes, err := g.fetchAll()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("git.fetchAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("git.fetchAll() = %v, want %v", gotRes, tt.wantRes)
+			}
+		})
+	}
+}
+
+func Test_git_revert(t *testing.T) {
+	type fields struct {
+		step *types.Step
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRes []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &git{
+				step: tt.fields.step,
+			}
+			gotRes, err := g.revert()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("git.revert() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("git.revert() = %v, want %v", gotRes, tt.wantRes)
+			}
+		})
+	}
+}
+
+func Test_git_checkout(t *testing.T) {
+	type fields struct {
+		step *types.Step
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRes []byte
+		wantErr bool
+	}{
+		{
+			name:    "Test_git_checkout_1",
+			fields:  fields{step: fakeGitOperator.Step()},
+			wantRes: nil,
+			wantErr: false,
+		},
+		{
+			name:    "Test_git_checkout_2",
+			fields:  fields{step: fakeErrGitOperator2.Step()},
+			wantRes: nil,
+			wantErr: true,
+		},
+		{
+			name:    "Test_git_checkout_3",
+			fields:  fields{step: fakeGitOperator2.Step()},
+			wantRes: nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &git{
+				step: tt.fields.step,
+			}
+			gotRes, err := g.checkout()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("git.checkout() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println("checkout to: ", g.step.Envs[types.PublisherGitBranch])
+			for _, v := range gotRes {
+				fmt.Printf("checkout out:%s", string(v))
+			}
+			//if !reflect.DeepEqual(gotRes, tt.wantRes) {
+			//	t.Errorf("git.checkout() = %v, want %v", gotRes, tt.wantRes)
+			//}
+		})
+	}
+}
+
+func Test_git_branch(t *testing.T) {
+	type fields struct {
+		step *types.Step
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRes string
+		wantErr bool
+	}{
+		{
+			name:    "Test_git_checkout_1",
+			fields:  fields{step: fakeGitOperator.Step()},
+			wantRes: "main",
+			wantErr: false,
+		},
+		{
+			name:    "Test_git_checkout_2",
+			fields:  fields{step: fakeGitOperator2.Step()},
+			wantRes: "test",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &git{
+				step: tt.fields.step,
+			}
+			if _, err := g.checkout(); err != nil {
+				t.Errorf("git.branch() g.checkout() error = %v", err)
+			}
+			gotRes, err := g.branch()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("git.branch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotRes != tt.wantRes {
+				t.Errorf("git.branch() = %v, want %v", gotRes, tt.wantRes)
+			}
 		})
 	}
 }
