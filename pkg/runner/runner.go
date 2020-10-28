62d4f8b34274a8e1567f6d6ce70f9a06b9043a2a
neverCase <1024769485@qq.com>
2020-10-28 11:30:02 +0800

    pkg/runner/runner.go

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,8 +1,14 @@
 package runner
 
 import (
+	"fmt"
 	"github.com/nevercase/publisher/pkg/interfaces"
 	"github.com/nevercase/publisher/pkg/types"
+	"k8s.io/klog"
+)
+
+const (
+	StepOperatorWasNotExisted = "err: the specific interfaces.StepOperator step-name:%s was not existed"
 )
 
 type Runner struct {
@@ -11,14 +17,41 @@ type Runner struct {
 	Namespace     string                    `json:"namespace" protobuf:"bytes,3,opt,name=namespace"`
 	GroupName     string                    `json:"groupName" protobuf:"bytes,4,opt,name=groupName"`
 	StepOperators []interfaces.StepOperator `json:"stepOperators" protobuf:"bytes,5,opt,name=stepOperators"`
+	StreamOutput  chan<- string             `json:"stream_output"`
 }
 
 func (r *Runner) Run(s *types.Step) (err error) {
-
+	exist := false
+	for _, v := range r.StepOperators {
+		if v.Step().Name == s.Name {
+			exist = true
+			res, err := v.Run(r.StreamOutput)
+			if err != nil {
+				klog.V(2).Info(err)
+				// todo report Run error
+				return err
+			}
+			// todo update Step information
+			_ = res
+		}
+	}
+	if !exist {
+		// todo report Run started failed
+		return fmt.Errorf(StepOperatorWasNotExisted, s.Name)
+	}
 	return nil
 }
 
 func (r *Runner) Update(s *types.Step) (err error) {
-
+	exist := false
+	for _, v := range r.StepOperators {
+		if v.Step().Name == s.Name {
+			exist = true
+			v.Step().Envs = s.Envs
+		}
+	}
+	if !exist {
+		return fmt.Errorf(StepOperatorWasNotExisted, s.Name)
+	}
 	return nil
 }
